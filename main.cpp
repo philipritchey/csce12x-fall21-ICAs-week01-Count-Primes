a0087b7084429ec4280fffaf51465014d5ea176f
Philip Ritchey <philipritchey@users.noreply.github.com>
2021-08-29 19:49:03 +0000

    added i/o specification.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+/*
+input: 0-terminated newline-delimited sequence of integers from standard input
+output: print to standard output the number of primes in the input sequence
+*/
+
 int main() {
   std::cout << "Hello World!\n";
 }
